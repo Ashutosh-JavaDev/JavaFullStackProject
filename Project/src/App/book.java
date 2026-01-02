54ef60e0009a5ce092ad1f5c8a2d86d7b94eed5c
Ashutosh Ayush <jbnashu297@gmail.com>
2026-01-02 20:59:34 +0530

    Libary Management System

--- Project/src/App/book.java
+++ Project/src/App/book.java
@@ -1,5 +1,20 @@
 package App;
 
 public class book {
-    
+    private String BookId;
+    private String title;
+    private String author;
+    private String category;
+    private String totalcopies;
+    private String availablecopies;
+    private boolean status;
+
+    private String BookId() {
+        return BookId;
+    }
+
+    private void setBookID(String BookId) {
+        this.BookId = BookId;
+
+    }
 }
