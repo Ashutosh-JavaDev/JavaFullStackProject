71c8abd1a3131f27c789c0c7fcda2a8f015956d2
Ashutosh Ayush <jbnashu297@gmail.com>
2026-01-02 21:36:05 +0530

    Libary Management System

--- Project/src/App/Book/Functionality.java
+++ Project/src/App/Book/Functionality.java
@@ -1,7 +1,10 @@
 package App.Book;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.InputMismatchException;
+
 public class Functionality {
     // add
     // update
@@ -9,5 +12,24 @@ public class Functionality {
     // search(by title,author,category)
     // view all books
     // view only available books
+    public void addBook(HashMap<String, String> bookname) {
+        bookname = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Book Name\nPress Enter to Exit");
+        String str = sc.nextLine();
+        while (true) {
+            if (str.equals(" ")) {
+                return;
+            } else {
+                try {
+                    System.out.println("Enter the Author Name");
+                    String authorName=sc.nextLine();
+                    bookname.put(str,authorName);
 
+                } catch (InputMismatchException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
