287bcc3de9345811914ae068c0b341945c76c0fa
Ashutosh Ayush <jbnashu297@gmail.com>
2026-01-02 20:53:06 +0530

    Libary Management System

--- Project/src/LibaryManagement.java
+++ Project/src/LibaryManagement.java
@@ -1,5 +1,11 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 public class LibaryManagement {
-    
+    public static void main(String[] args) {
+        HashMap<Integer,String>map=new HashMap<>();
+        System.out.println(map);
+        ArrayList<Integer>list=new ArrayList<>();
+        System.out.println(list);
+
+    }
 }
