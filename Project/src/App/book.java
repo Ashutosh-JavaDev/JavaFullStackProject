92dce70a216d0a2888154d5dc09b6fe6b45a0512
Ashutosh Ayush <jbnashu297@gmail.com>
2026-01-02 21:01:56 +0530

    Libary Management System

--- Project/src/App/book.java
+++ Project/src/App/book.java
@@ -9,12 +9,60 @@ public class book {
     private String availablecopies;
     private boolean status;
 
-    private String BookId() {
+    public String getBookId() {
         return BookId;
     }
 
-    private void setBookID(String BookId) {
-        this.BookId = BookId;
-
+    public void setBookId(String bookId) {
+        BookId = bookId;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTotalcopies() {
+        return totalcopies;
+    }
+
+    public void setTotalcopies(String totalcopies) {
+        this.totalcopies = totalcopies;
+    }
+
+    public String getAvailablecopies() {
+        return availablecopies;
+    }
+
+    public void setAvailablecopies(String availablecopies) {
+        this.availablecopies = availablecopies;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
 }
