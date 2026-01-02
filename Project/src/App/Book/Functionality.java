e1cff94d3b4b7096a829b7d578093b0f1b4483a3
Ashutosh Ayush <jbnashu297@gmail.com>
2026-01-02 21:21:41 +0530

    Libary Management System

--- Project/src/App/Book/Functionality.java
+++ Project/src/App/Book/Functionality.java
@@ -1,5 +1,13 @@
 package App.Book;
-
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
 public class Functionality {
-    
+    // add
+    // update
+    // delete
+    // search(by title,author,category)
+    // view all books
+    // view only available books
+
 }
