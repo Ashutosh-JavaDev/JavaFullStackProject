6f4031f0fcdc946b0104870911dff825956d899c
Ashutosh Ayush <jbnashu297@gmail.com>
2026-01-03 22:37:56 +0530

    Libary Management System

--- Project/src/LibaryManagement.java
+++ Project/src/LibaryManagement.java
@@ -1,11 +1,11 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import App.Book.Functionality;
+
 public class LibaryManagement {
     public static void main(String[] args) {
-        HashMap<Integer,String>map=new HashMap<>();
-        System.out.println(map);
-        ArrayList<Integer>list=new ArrayList<>();
-        System.out.println(list);
+      Functionality function=new Functionality();
+      while(true){
+        System.out.println("Press 1.To Add\nPress 2. To Update\nPress 3. To Delete\nPress 4. To Search\nPress 5. To View all Book\nPress 6. To View Available Books");
+      }
 
     }
 }
